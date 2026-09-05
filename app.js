118fbbe05b65a8035206973b8ad3e5b2d0d11d25
patusiintegratedfarming-hash <patusiintegratedfarming@gmail.com>
2026-09-05 04:00:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     module.addEventListener("click", function () {
       alert("Farm Overview clicked");
       const title = module.querySelector("h3");
-
+alert("Title is: [" + title.textContent.trim() + "]");
       if (title && title.textContent.trim() === "Farm Overview") {
         alert("Page found: " + !!document.getElementById("farm-overview-page"));
         document.getElementById("farm-overview-page").style.display = "block";
